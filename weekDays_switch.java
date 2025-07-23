ef6cff9b1673f463fbd132702defd57fab08ec9f
Parvathy J Nair <parvathyj06@gmail.com>
2025-07-23 09:53:16 +0530

    Add files via upload

---
+++ weekDays_switch.java
@@ -0,0 +1,36 @@
+package java_lab;
+import java.util.Scanner;
+public class weekDays_switch {
+	public static void main(String [] args) {
+		Scanner sc=new Scanner (System.in);
+		System.out.println("Enter the number:");
+		int days=sc.nextInt();
+		switch(days) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5 :
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		case 7:
+			System.out.println("Sunday");
+			break;
+		default :
+			System.out.println("Error");
+			break;
+		}
+	}
+
+}
