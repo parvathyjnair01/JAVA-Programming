4352def19bb17e86024826d7b5e4441e49ea9f63
Parvathy J Nair <parvathyj06@gmail.com>
2025-07-23 10:03:44 +0530

    Add files via upload

---
+++ odd_numbers.java
@@ -0,0 +1,19 @@
+package java_lab;
+import java.util.Scanner;
+
+public class odd_numbers {
+	public static void main(String [] args) {
+		for(int i = 1;i <50;i ++) {
+			if(i%2==0) {
+				continue;
+			}
+			else {
+				System.out.println(+i);
+			}
+			
+		}
+
+	
+	}
+
+}
